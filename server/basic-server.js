edbdeb0a9bc7645f75734988b06167cbcfba0986
Khai Tran <kqtran917@yahoo.com>
2021-10-08 12:47:17 -0500

    Implemented export of request-handler.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,3 +1,5 @@
+var {handleRequest} = require('./request-handler.js');
+
 /* Import node's http module: */
 var http = require('http');
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -31,8 +31,23 @@ var requestHandler = function(request, response) {
 
   // The outgoing status.
   var statusCode = 200;
+  // These headers will allow Cross-Origin Resource Sharing (CORS).
+  // This code allows this server to talk to websites that
+  // are on different domains, for instance, your chat client.
+  //
+  // Your chat client is running from a url like file://your/chat/client/index.html,
+  // which is considered a different domain.
+  //
+  // Another way to get around this restriction is to serve you chat
+  // client from this domain by setting up static file serving.
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
 
-  // See the note below about CORS headers.
+  // See the note above about CORS headers.
   var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
@@ -55,19 +70,6 @@ var requestHandler = function(request, response) {
   response.end('Hello, World!');
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+exports.handleRequest = requestHandler;
 
