20329cfb30c4ad379a6f210e8218e126c5bd606c
Khai Tran <kqtran917@yahoo.com>
2021-10-09 12:50:56 -0500

    Completed express refactor. Deleted unncessesary files

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,17 +1,14 @@
-var {requestHandler} = require('./request-handler.js');
-
 var express = require('express');
 var cors = require('cors');
-// const bodyParser = require('body-parser');
 
 var app = express();
 var port = 3000;
 
-var messages = [];
+
 app.use(cors());
-// app.use(bodyParser.urlencoded({extended: false }));
 app.use(express.json());
 
+var messages = [];
 
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
@@ -41,36 +38,12 @@ app.get('/classes/messages', (req, res) => {
 });
 
 app.post('/classes/messages', (req, res) => {
-  console.log(req.body);
+  messages.push(req.body);
+  console.log(messages);
+  var statusCode = 201;
+  res.writeHead(statusCode, headers);
+  res.end();
 });
-
-
-
-//  if (request.url === '/classes/messages') {
-
-//   if (request.method === 'OPTIONS') {
-//     var statusCode = 200;
-//     response.writeHead(statusCode, headers);
-//     response.end();
-
-//   } else if (request.method === 'POST') {
-//     var body = [];
-//     request.on('data', (data) => {
-//       body.push(data);
-//     }).on('end', () => {
-//       body = Buffer.concat(body).toString();
-//       messages.push(JSON.parse(body));
-//       var statusCode = 201;
-//       response.writeHead(statusCode, headers);
-//       response.end();
-//     });
-//   }
-// } else {
-//   var statusCode = 404;
-//   response.writeHead(statusCode, headers);
-//   response.end();
-// }
-
 
 
 app.listen(port, () => {
